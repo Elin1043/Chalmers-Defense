9fdc3618db5d7984cab891b5059d5e649793cdf7
Elin1043 <47697397+Elin1043@users.noreply.github.com>
2021-09-01 09:48:25 +0200

    Test på workshop

--- core/src/com/mygdx/chalmersdefense/Launcher.java
+++ core/src/com/mygdx/chalmersdefense/Launcher.java
@@ -21,6 +21,7 @@ public class Launcher extends ApplicationAdapter {
 		music.setLooping(true);
 		music.setVolume((float) 0.2);
 		music.play();
+		System.out.println("Test");
 
 		Gdx.graphics.setWindowedMode(1280, 900); // Sets the width and height of the program window
 	}
