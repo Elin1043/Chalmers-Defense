4ec2e04bd3d4942ef93259d59cc5409b10f8727d
Turbobus <59963570+Turbobus@users.noreply.github.com>
2021-09-16 01:43:05 +0200

    Started creating system for spawning viruses in predefined rounds

--- core/src/com/mygdx/chalmersdefense/Model/SpawnViruses.java
+++ core/src/com/mygdx/chalmersdefense/Model/SpawnViruses.java
@@ -1,5 +1,7 @@
 package com.mygdx.chalmersdefense.Model;
 
+import java.util.List;
+
 /**
  * @author Joel Båtsman Hilmersson
  * A class that controlls the spawning of viruses for the different rounds
@@ -7,6 +9,26 @@ package com.mygdx.chalmersdefense.Model;
 public class SpawnViruses {
 
     private final String[][] spawnInfo = {{"1|100", "1|80", "1|60", "1|40", "1|40", "1|60", "1|80", "1|100"}};
+    private final List<Virus> listToAddTo;
 
+    String[] currentRound;
+    private int waveIndex = 0;
 
+    public SpawnViruses(List<Virus> listToAddTo) {
+        this.listToAddTo = listToAddTo;
+    }
+
+    public void spawnRound(int round, List<Virus> listToAddTo) {
+        currentRound = spawnInfo[round - 1];
+        waveIndex = 0;
+
+    }
+
+    private void parseRound(){
+        String[] splitedWave = currentRound[waveIndex].split("[|]");
+
+//        switch (splitedWave[0]){
+//            //case 1 -> listToAddTo.add(VirusFactory.createVirusOne());
+//        }
+    }
 }
