145ce30ceddff958a7c1302d2e52b84f6d74a1fd
Daniel <perssondaniel01@gmail.com>
2021-09-15 10:40:02 +0200

    Added Skin to button

--- core/src/com/mygdx/chalmersdefense/Controllers/RightSidePanelController.java
+++ core/src/com/mygdx/chalmersdefense/Controllers/RightSidePanelController.java
@@ -2,33 +2,35 @@ package com.mygdx.chalmersdefense.Controllers;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.mygdx.chalmersdefense.Views.GameScreen;
 
 public class RightSidePanelController {
     private Stage stage;
+    private Button startRoundButton;
 
     public RightSidePanelController(Stage stage) {
         this.stage = stage;
-        createPlayButton(stage);
+        createStartRoundButton();
     }
 
-    private void createPlayButton(Stage stage) {
-        Texture playButtonTexture = new Texture(Gdx.files.internal("playButton.png"));
-        TextureRegion playButtonTextureRegion = new TextureRegion(playButtonTexture);
-        TextureRegionDrawable playTexRegDrawable = new TextureRegionDrawable(playButtonTextureRegion);
-        ImageButton playButton = new ImageButton(playTexRegDrawable); //Set the button up
-        playButton.setPosition(1500, 20);
+    private void createStartRoundButton() {
+        TextureAtlas atlas = new TextureAtlas(Gdx.files.internal("buttons/startRoundButtonSkin/startRoundButtonSkin.atlas")); // Load atlas file from skin
+        Skin skin = new Skin(Gdx.files.internal("buttons/startRoundButtonSkin/startRoundButtonSkin.json"), atlas); // Create skin object
+        startRoundButton = new Button(skin);
+        startRoundButton.setPosition(1500, 20);
 
-        stage.addActor(playButton); //Add the button to the stage to perform rendering and take input.
+        stage.addActor(startRoundButton); //Add the button to the stage to perform rendering and take input.
 
-        playButton.addListener(new ClickListener() {
-
+        startRoundButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 // Call model.
