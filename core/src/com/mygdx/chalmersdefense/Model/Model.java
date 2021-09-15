2436c238692431164f73d338629b8a05c1850c4d
Daniel <perssondaniel01@gmail.com>
2021-09-16 00:39:06 +0200

    Added Rounds and changed access modifiers

--- core/src/com/mygdx/chalmersdefense/Model/Model.java
+++ core/src/com/mygdx/chalmersdefense/Model/Model.java
@@ -5,14 +5,16 @@ import com.mygdx.chalmersdefense.Views.GameScreen;
 import com.mygdx.chalmersdefense.Views.MainScreen;
 
 public class Model {
-    MainScreen mainScreen;
-    GameScreen gameScreen;
-    ChalmersDefense game;
+    private MainScreen mainScreen;
+    private GameScreen gameScreen;
+    private ChalmersDefense game;
+    private Rounds rounds;
 
     public Model(ChalmersDefense game, MainScreen mainScreen, GameScreen gameScreen) {
         this.game = game;
         this.mainScreen = mainScreen;
         this.gameScreen = gameScreen;
+        this.rounds = new Rounds(20);
     }
 
     public void playButtonAction() {
@@ -22,5 +24,6 @@ public class Model {
 
     public void startRoundButtonAction() {
         // Do something
+        rounds.sendNextRound();
     }
 }
