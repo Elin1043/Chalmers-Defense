a403f62b5034d8330c76923fc02d21d43ad0760e
Elin1043 <47697397+Elin1043@users.noreply.github.com>
2021-09-15 14:20:04 +0200

    Added classes for vectors, tower and objects on the map

---
+++ core/src/com/mygdx/chalmersdefense/Models/MapObject.java
@@ -0,0 +1,9 @@
+package com.mygdx.chalmersdefense.Models;
+
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.mygdx.chalmersdefense.Vectors;
+
+public class MapObject {
+    public MapObject(Vectors pos, Image image, int angle) {
+    }
+}
---
+++ core/src/com/mygdx/chalmersdefense/Models/Tower.java
@@ -0,0 +1,66 @@
+package com.mygdx.chalmersdefense.Models;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.MapObjects;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.mygdx.chalmersdefense.Vectors;
+
+public abstract class Tower{
+    private final Vectors pos;
+    private final Texture texture;
+    private final int angle;
+    private double range;
+    private String name;
+    private Tower upgrade;
+    private boolean isPlaced=false;
+
+    private int attackDamage;
+    private int attackSpeed;
+
+
+
+    public Tower(Vectors pos, Texture texture, int angle, String name, int attackDamage, int attackSpeed) {
+        this.pos = pos;
+        this.texture = texture;
+        this.angle = angle;
+        this.name=name;
+        this.attackDamage = attackDamage;
+        this.attackSpeed = attackSpeed;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public Texture getTexture(){
+        return texture;
+    }
+
+    public Vectors getPos(){
+        return pos;
+    }
+
+    public int getAngle(){
+        return angle;
+    }
+
+    public double getRange(){
+        return range;
+    }
+
+    public boolean isPlaced(){
+        return isPlaced;
+    }
+    public void setPlaced(boolean placed){
+        isPlaced=placed;
+    }
+    public int getAttackDamage() {
+        return attackDamage;
+    }
+
+    public int getAttackSpeed() {
+        return attackSpeed;
+    }
+
+
+}
