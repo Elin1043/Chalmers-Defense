840a3f34a61e28eb208531bc4e95704c3190aedd
Daniel <perssondaniel01@gmail.com>
2021-09-15 17:04:24 +0200

    Added setup for Model

---
+++ core/src/com/mygdx/chalmersdefense/Model/Model.java
@@ -0,0 +1,22 @@
+package com.mygdx.chalmersdefense.Model;
+
+import com.mygdx.chalmersdefense.ChalmersDefense;
+import com.mygdx.chalmersdefense.Views.GameScreen;
+import com.mygdx.chalmersdefense.Views.MainScreen;
+
+public class Model {
+    MainScreen mainScreen;
+    GameScreen gameScreen;
+    ChalmersDefense game;
+
+    public Model(ChalmersDefense game, MainScreen mainScreen, GameScreen gameScreen) {
+        this.game = game;
+        this.mainScreen = mainScreen;
+        this.gameScreen = gameScreen;
+    }
+
+    public void playButtonAction() {
+        // Do something
+        game.setScreen(gameScreen);
+    }
+}
