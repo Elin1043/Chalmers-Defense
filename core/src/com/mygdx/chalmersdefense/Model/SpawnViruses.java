2e7d908c3d58ea6b40cfef4728530ac8af12cf50
Turbobus <59963570+Turbobus@users.noreply.github.com>
2021-09-17 13:38:33 +0200

    You can now spawn multiple of the same viruses using one command

--- core/src/com/mygdx/chalmersdefense/Model/SpawnViruses.java
+++ core/src/com/mygdx/chalmersdefense/Model/SpawnViruses.java
@@ -12,13 +12,14 @@ import java.util.concurrent.TimeUnit;
  */
 public class SpawnViruses {
 
-    private final String[][] spawnInfo = {{"1|3000", "2|500", "3|500", "4|1000", "5|1000", "5|500", "1|500", "2|1000"}};
+    private final String[][] spawnInfo = {{"1|3000", "2*20|250|3000", "3|500", "4|1000", "5|1000", "5|500", "1|500", "2|1000"}};
     private final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
     private List<Virus> listToAddTo;
     private boolean isSpawning = false;
 
     String[] currentRound;
     private int waveIndex = 0;
+    private int waveAmountSpawned;
 
     public SpawnViruses(List<Virus> listToAddTo) {
         this.listToAddTo = listToAddTo;
@@ -26,7 +27,6 @@ public class SpawnViruses {
 
     public void spawnRound(int round) {
         if (!isSpawning) {
-            System.out.println("HEJ");
             currentRound = spawnInfo[round - 1];
             waveIndex = 0;
             isSpawning = true;
@@ -42,19 +42,44 @@ public class SpawnViruses {
             String[] splitedWave = currentRound[waveIndex].split("[|]");
 
 
-            switch (Integer.parseInt(splitedWave[0])) {
-                case 1 -> listToAddTo.add(VirusFactory.createVirusOne());     // Måste ha blivit kallat 1 gång utan executorservice, annars slutar den gå eftersom den inte kan skapa en ny textur
-                case 2 -> listToAddTo.add(VirusFactory.createVirusTwo());
-                case 3 -> listToAddTo.add(VirusFactory.createVirusThree());
-                case 4 -> listToAddTo.add(VirusFactory.createVirusFour());
-                case 5 -> listToAddTo.add(VirusFactory.createVirusFive());
-            }
+            if (splitedWave.length == 2) {
+                switch (Integer.parseInt(splitedWave[0])) {
+                    case 1 -> listToAddTo.add(VirusFactory.createVirusOne());     // Måste ha blivit kallat 1 gång utan executorservice, annars slutar den gå eftersom den inte kan skapa en ny textur
+                    case 2 -> listToAddTo.add(VirusFactory.createVirusTwo());
+                    case 3 -> listToAddTo.add(VirusFactory.createVirusThree());
+                    case 4 -> listToAddTo.add(VirusFactory.createVirusFour());
+                    case 5 -> listToAddTo.add(VirusFactory.createVirusFive());
+                    default -> waveIndex = currentRound.length;                 // Maybe exception instead
+                }
+                executorService.schedule(this::parseRound, Integer.parseInt(splitedWave[1]), TimeUnit.MILLISECONDS);
+                waveIndex++;
+            } else if (splitedWave.length == 3) {
+                String[] spawnInfo = splitedWave[0].split("[*]");
 
-            waveIndex++;
-            executorService.schedule(this::parseRound, Integer.parseInt(splitedWave[1]), TimeUnit.MILLISECONDS);
+                if (waveAmountSpawned < Integer.parseInt(spawnInfo[1])) {
+                    switch (Integer.parseInt(spawnInfo[0])) {
+                        case 1 -> listToAddTo.add(VirusFactory.createVirusOne());     // Måste ha blivit kallat 1 gång utan executorservice, annars slutar den gå eftersom den inte kan skapa en ny textur
+                        case 2 -> listToAddTo.add(VirusFactory.createVirusTwo());
+                        case 3 -> listToAddTo.add(VirusFactory.createVirusThree());
+                        case 4 -> listToAddTo.add(VirusFactory.createVirusFour());
+                        case 5 -> listToAddTo.add(VirusFactory.createVirusFive());
+                        default -> waveIndex = currentRound.length;                 // Maybe exception instead
+                    }
+                    waveAmountSpawned++;
+
+                    System.out.println(splitedWave[1]);
+                    executorService.schedule(this::parseRound, Integer.parseInt(splitedWave[1]), TimeUnit.MILLISECONDS);
+                } else {
+                    waveAmountSpawned = 0;
+                    executorService.schedule(this::parseRound, Integer.parseInt(splitedWave[2]), TimeUnit.MILLISECONDS);
+                    waveIndex++;
+                }
+
+            }
 
         } else {
             waveIndex = 0;
+            waveAmountSpawned = 0;
             isSpawning = false;
         }
     }
