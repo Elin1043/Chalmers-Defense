ba393ba257c5583c46188d246aa80126237adb8d
Daniel <perssondaniel01@gmail.com>
2021-09-17 17:17:16 +0200

    No longer in charge of screen switching.

--- core/src/com/mygdx/chalmersdefense/Model/Model.java
+++ core/src/com/mygdx/chalmersdefense/Model/Model.java
@@ -1,22 +1,11 @@
 package com.mygdx.chalmersdefense.Model;
 
 import com.mygdx.chalmersdefense.ChalmersDefense;
-import com.mygdx.chalmersdefense.Views.GameScreen;
-import com.mygdx.chalmersdefense.Views.MainScreen;
 
 public class Model {
-    MainScreen mainScreen;
-    GameScreen gameScreen;
     ChalmersDefense game;
 
-    public Model(ChalmersDefense game, MainScreen mainScreen, GameScreen gameScreen) {
+    public Model(ChalmersDefense game) {
         this.game = game;
-        this.mainScreen = mainScreen;
-        this.gameScreen = gameScreen;
-    }
-
-    public void playButtonAction() {
-        // Do something
-        game.setScreen(gameScreen);
     }
 }
