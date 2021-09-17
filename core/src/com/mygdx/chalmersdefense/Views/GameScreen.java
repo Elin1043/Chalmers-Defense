b1ad996114fccf247a12002c84196bddd9e26e98
Daniel <perssondaniel01@gmail.com>
2021-09-17 17:14:17 +0200

    GameScreen now extends AbstractScreen.

--- core/src/com/mygdx/chalmersdefense/Views/GameScreen.java
+++ core/src/com/mygdx/chalmersdefense/Views/GameScreen.java
@@ -1,63 +1,23 @@
 package com.mygdx.chalmersdefense.Views;
 
-import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.utils.viewport.Viewport;
-import com.mygdx.chalmersdefense.ChalmersDefense;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
-public class GameScreen implements Screen {
+public class GameScreen extends AbstractScreen implements Screen {
 
-    private final ChalmersDefense game;
-    Viewport viewport;
-    Batch batch;
+    Image virus;
 
-    Sprite virus;
-
-    public GameScreen(ChalmersDefense game, Batch batch, Viewport viewport){
-        this.game = game;
-        this.viewport = viewport;
-        this.batch = batch;
-
-        virus = new Sprite(new Texture("corona_virus_low.png"));
-
+    public GameScreen(){
+        super();
+        virus = new Image(new Texture("corona_virus_low.png"));
         virus.setPosition(-300, -150);	// This needs to be fixed with later sprites
     }
 
     @Override
-    public void show() {
-
-    }
-
-    @Override
-    public void render(float delta) {
-        virus.draw(batch);
-    }
-
-    @Override
-    public void resize(int width, int height) {
-
-    }
-
-    @Override
-    public void pause() {
-
-    }
-
-    @Override
-    public void resume() {
-
-    }
-
-    @Override
-    public void hide() {
-
-    }
-
-    @Override
-    public void dispose() {
-
+    public void buildStage() {
+        addActor(virus);
+        super.render(Gdx.graphics.getDeltaTime());
     }
 }
