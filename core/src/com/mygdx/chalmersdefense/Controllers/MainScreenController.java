81f737a2d2e6d086d57fd524cbd00c8b79bd9d0e
Daniel <perssondaniel01@gmail.com>
2021-09-17 17:23:30 +0200

    Removed UI button creation and added a listener adder

--- core/src/com/mygdx/chalmersdefense/Controllers/MainScreenController.java
+++ core/src/com/mygdx/chalmersdefense/Controllers/MainScreenController.java
@@ -5,38 +5,25 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.mygdx.chalmersdefense.Model.Model;
+import com.mygdx.chalmersdefense.Views.ScreenEnum;
+import com.mygdx.chalmersdefense.Views.ScreenManager;
 
 public class MainScreenController {
-    private ImageButton playButton;
-    private Model model;
-    private Stage stage;
 
-    public MainScreenController(Model model) {
-        this.model = model;
+    public MainScreenController() {
+
     }
 
-    public void addStage(Stage stage) {
-        this.stage = stage;
-        createPlayButton();
-    }
-
-    private void createPlayButton(){
-        Texture playButtonTexture = new Texture(Gdx.files.internal("playButton.png"));
-        TextureRegion playButtonTextureRegion = new TextureRegion(playButtonTexture);
-        TextureRegionDrawable playTexRegDrawable = new TextureRegionDrawable(playButtonTextureRegion);
-        playButton = new ImageButton(playTexRegDrawable); //Set the button up
-        playButton.setPosition(832, 20);
-
-        stage.addActor(playButton); //Add the button to the stage to perform rendering and take input.
-
-        playButton.addListener(new ClickListener() {
+    public void addPlayButtonListener(Button button) {
+        button.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                model.playButtonAction();
+                ScreenManager.getInstance().showScreen(ScreenEnum.GAME);
             }
         });
     }
