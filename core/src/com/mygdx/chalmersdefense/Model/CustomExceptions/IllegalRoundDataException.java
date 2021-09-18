0c74fefff688c11c95db8601f0140077765f38d0
Turbobus <59963570+Turbobus@users.noreply.github.com>
2021-09-18 18:21:42 +0200

    Created the IllegalRoundDataException constructor

--- core/src/com/mygdx/chalmersdefense/Model/CustomExceptions/IllegalRoundDataException.java
+++ core/src/com/mygdx/chalmersdefense/Model/CustomExceptions/IllegalRoundDataException.java
@@ -2,4 +2,7 @@ package com.mygdx.chalmersdefense.Model.CustomExceptions;
 
 public class IllegalRoundDataException extends Exception{
 
+    public IllegalRoundDataException(String message) {
+        super(message);
+    }
 }
