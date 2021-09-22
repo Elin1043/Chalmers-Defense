a377ec5d2733ff2308ce74741da40ece3154537a
Elin1043 <47697397+Elin1043@users.noreply.github.com>
2021-09-22 10:33:40 +0200

    Adjusted coordinates

--- core/src/com/mygdx/chalmersdefense/Model/Path/GamePaths/ClassicPath.java
+++ core/src/com/mygdx/chalmersdefense/Model/Path/GamePaths/ClassicPath.java
@@ -17,16 +17,16 @@ public class ClassicPath extends Path {
     @Override
     protected void setPathWaypoints() {
         if (pathWaypoints.isEmpty()) {
-            pathWaypoints.add(new PositionVector(0, 472));
-            pathWaypoints.add(new PositionVector(476, 472));
-            pathWaypoints.add(new PositionVector(476, 756));
+            pathWaypoints.add(new PositionVector(0, 456));
+            pathWaypoints.add(new PositionVector(483, 456));
+            pathWaypoints.add(new PositionVector(483, 756));
             pathWaypoints.add(new PositionVector(270, 756));
             pathWaypoints.add(new PositionVector(270, 973));
             pathWaypoints.add(new PositionVector(633, 973));
             pathWaypoints.add(new PositionVector(633, 323));
             pathWaypoints.add(new PositionVector(1379, 323));
-            pathWaypoints.add(new PositionVector(1379, 917));
-            pathWaypoints.add(new PositionVector(1081, 917));
+            pathWaypoints.add(new PositionVector(1379, 901));
+            pathWaypoints.add(new PositionVector(1081, 901));
             pathWaypoints.add(new PositionVector(1081, 684));
             pathWaypoints.add(new PositionVector(861, 684));
             pathWaypoints.add(new PositionVector(861, 1080));
