4c804b8114ca7bbf4d4aa18e8a642d9af7a2661e
Turbobus <59963570+Turbobus@users.noreply.github.com>
2021-09-23 21:32:45 +0200

    Added Player class to hold methods around lives and money in the game

---
+++ core/src/com/mygdx/chalmersdefense/Model/Player.java
@@ -0,0 +1,21 @@
+package com.mygdx.chalmersdefense.Model;
+
+/**
+ * @author Joel Båtsman Hilmersson
+ * Class representing the player and thier resorces
+ */
+class Player {
+
+    private int lives;
+    private int money;
+
+    Player(int lives, int startCapital){
+        this.lives = lives;
+        money = startCapital;
+    }
+
+
+    int getLives() { return lives; }
+
+
+}
