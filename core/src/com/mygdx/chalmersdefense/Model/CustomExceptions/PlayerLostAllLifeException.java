7eb8cb7934fc88b11d0b7036096535475088c44a
Turbobus <59963570+Turbobus@users.noreply.github.com>
2021-09-24 16:58:00 +0200

    Added exception class for when player loses all lives

---
+++ core/src/com/mygdx/chalmersdefense/Model/CustomExceptions/PlayerLostAllLifeException.java
@@ -0,0 +1,7 @@
+package com.mygdx.chalmersdefense.Model.CustomExceptions;
+
+public class PlayerLostAllLifeException extends Exception {
+    public PlayerLostAllLifeException(String message) {
+        super(message);
+    }
+}
