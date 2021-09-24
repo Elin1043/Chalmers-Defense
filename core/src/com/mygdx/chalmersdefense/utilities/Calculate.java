6964594c6452a1e92793893629a75e20de083120
Turbobus <59963570+Turbobus@users.noreply.github.com>
2021-09-24 23:22:13 +0200

    Fixed the problem with angle calculation for towers

--- core/src/com/mygdx/chalmersdefense/utilities/Calculate.java
+++ core/src/com/mygdx/chalmersdefense/utilities/Calculate.java
@@ -35,10 +35,12 @@ public abstract class Calculate {
 
     public static float angleDeg (float rotToX, float rotToY, float orgX, float orgY) {
         float angle = (float) Math.toDegrees(Math.atan2(rotToY - orgY, rotToX - orgX));
+
         if(angle < 0){
             angle += 360;
         }
-        return angle - 45;
+
+        return angle;
     }
 
 
