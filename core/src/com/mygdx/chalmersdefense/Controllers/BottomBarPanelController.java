69b722b6d783e7cce135eca2d5960a0ff417272a
Daniel <perssondaniel01@gmail.com>
2021-09-24 17:01:57 +0200

    Added controller class for game screens bottom bar.

---
+++ core/src/com/mygdx/chalmersdefense/Controllers/BottomBarPanelController.java
@@ -0,0 +1,32 @@
+package com.mygdx.chalmersdefense.Controllers;
+
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.mygdx.chalmersdefense.Model.Model;
+
+/*
+* @author Daniel Persson
+*
+* */
+public class BottomBarPanelController {
+    private Model model;
+
+    public BottomBarPanelController(Model model) {
+        this.model = model;
+    }
+
+    /*
+    * @author Daniel Persson
+    *
+    * Method for adding click listener to upgrade button
+    * */
+    public void addClickListenerUpgradeButton(Button button) {
+        button.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                // Do something
+            }
+        });
+    }
+}
