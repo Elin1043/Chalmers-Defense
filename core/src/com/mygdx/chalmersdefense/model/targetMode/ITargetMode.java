535efe5d597b4d1e7c82d1378ec32f60b6a46fd7
Turbobus <59963570+Turbobus@users.noreply.github.com>
2021-09-24 20:46:28 +0200

    Added ITargetMode interface to be used by different targetingmodes

---
+++ core/src/com/mygdx/chalmersdefense/model/targetMode/ITargetMode.java
@@ -0,0 +1,10 @@
+package com.mygdx.chalmersdefense.model.targetMode;
+
+/**
+ * @author Joel Båtsman Hilmersson
+ *
+ * Interface for every targeting mode to use
+ */
+public interface ITargetMode {
+
+}
