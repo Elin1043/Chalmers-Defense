c4bfe07dac7db2b362cf1d949da4d35a2033c776
Elin1043 <47697397+Elin1043@users.noreply.github.com>
2021-09-25 14:37:11 +0200

    Removed unused import

--- core/src/com/mygdx/chalmersdefense/model/towers/EcoTower.java
+++ core/src/com/mygdx/chalmersdefense/model/towers/EcoTower.java
@@ -1,6 +1,5 @@
 package com.mygdx.chalmersdefense.model.towers;
 
-import com.mygdx.chalmersdefense.model.projectiles.BulletProjectile;
 import com.mygdx.chalmersdefense.model.projectiles.Projectile;
 import com.mygdx.chalmersdefense.model.targetMode.ITargetMode;
 
@@ -11,7 +10,7 @@ public class EcoTower extends Tower{
 
     int moneyEarned;
     int currentReload;
-    private int reloadTime = 60*5; //how many updates from model
+    private int reloadTime = 60*10; //how many updates from model
 
     public EcoTower(float x, float y, String name, int attackSpeed, int cost, int range, List<ITargetMode> targetModes) {
         super(x, y, name, attackSpeed, cost, range, targetModes);
