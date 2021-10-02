380f653454331e69c5f54d40dc2727e98dff5641
Daniel <perssondaniel01@gmail.com>
2021-10-03 00:40:46 +0200

    Added text to panel

--- core/src/com/mygdx/chalmersdefense/views/GameScreenViews/LostPanel.java
+++ core/src/com/mygdx/chalmersdefense/views/GameScreenViews/LostPanel.java
@@ -7,6 +7,8 @@ import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.utils.Align;
+import com.mygdx.chalmersdefense.controllers.GameScreenController;
 import com.mygdx.chalmersdefense.utilities.FontFactory;
 
 
@@ -16,6 +18,7 @@ import com.mygdx.chalmersdefense.utilities.FontFactory;
  */
 public class LostPanel {
     private Stage stage;
+    private GameScreenController gameScreenController;
     private final float WIDTH = 810;
     private final float HEIGHT = 400;
 
@@ -27,20 +30,28 @@ public class LostPanel {
     private final Image backgroundImage = new Image(new Texture("GameScreen/LostPanelBackgroundImage.png"));
 
     private final Label title = new Label("You Lost :'(", FontFactory.getLabelStyle36BlackBold());
-    private final Label mainText = new Label("You Lost :'(", FontFactory.getLabelStyle36BlackBold());
-    private final Button mainMenuButton = new Button(lostButtonSkin);
-    private final Button tryAgainButton = new Button(lostButtonSkin);
+    private final Label mainText = new Label("The evil Corona viruses have won. Chalmers have fallen and will now be the center of the new wave of even more powerful virus variants.", FontFactory.getLabelStyle18Black());
 
-    public LostPanel(Stage stage) {
+    private final Button mainMenuButton = new Button(lostButtonSkin);
+    private final Label mainMenuButtonText = new Label("Main menu", FontFactory.getLabelStyle24BlackSemiBold());
+
+    private final Button tryAgainButton = new Button(lostButtonSkin);
+    private final Label tryAgainButtonText = new Label("Try again", FontFactory.getLabelStyle24BlackSemiBold());
+
+    public LostPanel(Stage stage, GameScreenController gameScreenController) {
         this.stage = new Stage(stage.getViewport());
+        this.gameScreenController = gameScreenController;
     }
 
     public void initialize() {
         stage.addActor(lostPanelGroup);
         lostPanelGroup.addActor(backgroundImage);
         lostPanelGroup.addActor(title);
+        lostPanelGroup.addActor(mainText);
         lostPanelGroup.addActor(mainMenuButton);
+        mainMenuButton.addActor(mainMenuButtonText);
         lostPanelGroup.addActor(tryAgainButton);
+        tryAgainButton.addActor(tryAgainButtonText);
 
         backgroundImage.setPosition(stage.getWidth()/2 - WIDTH/2, stage.getHeight()/2 - HEIGHT/2);
 
@@ -48,19 +59,31 @@ public class LostPanel {
             backgroundImage.getX() + backgroundImage.getWidth() / 2 - title.getWidth() / 2,
             backgroundImage.getY() + backgroundImage.getHeight() - 100);
 
+        mainText.setWrap(true);
+        mainText.setWidth(720);
+        mainText.setAlignment(Align.center);
+        mainText.setPosition(
+                backgroundImage.getX() + backgroundImage.getWidth() / 2 - 720 / 2,
+                backgroundImage.getY() + backgroundImage.getHeight() - 150);
+
+        gameScreenController.addLostPanelMainMenuClickListener(mainMenuButton);
         mainMenuButton.setPosition(
             backgroundImage.getX() + backgroundImage.getWidth()/4 - mainMenuButton.getWidth()/2,
-            backgroundImage.getY() + 100);
+            backgroundImage.getY() + 65);
+        mainMenuButtonText.setPosition(mainMenuButton.getWidth()/2 - mainMenuButtonText.getWidth()/2, mainMenuButton.getHeight()/2 - mainMenuButtonText.getHeight()/2 + 5);
 
         tryAgainButton.setPosition(
             backgroundImage.getX() + backgroundImage.getWidth() * 3/4 - mainMenuButton.getWidth()/2,
-            backgroundImage.getY() + 100);
+            backgroundImage.getY() + 65);
+        tryAgainButtonText.setPosition(tryAgainButton.getWidth()/2 - tryAgainButtonText.getWidth()/2, tryAgainButton.getHeight()/2 - tryAgainButtonText.getHeight()/2 + 5);
 
         lostPanelGroup.setVisible(false);
     }
 
     public void render() {
         Gdx.input.setInputProcessor(stage);
+
+
         stage.act();
         stage.draw();
 
