af85ee8b847b8927227edeb484aea87a0f96277c
Daniel <perssondaniel01@gmail.com>
2021-10-04 00:41:38 +0200

    Added starting round test for Rounds class

---
+++ core/src/test/java/TestRounds.java
@@ -0,0 +1,25 @@
+import static org.junit.Assert.*;
+
+import com.mygdx.chalmersdefense.ChalmersDefense;
+import com.mygdx.chalmersdefense.model.Model;
+import org.junit.*;
+
+/**
+ * @author Daniel Persson
+ */
+public class TestRounds {
+    private ChalmersDefense game;
+    private Model model;
+
+    @Before
+    public void init() {
+        game = new ChalmersDefense();
+        model = new Model(game);
+    }
+
+    @Test
+    public void testStartingRound() {
+        assertEquals(1, model.getCurrentRound());
+    }
+
+}
