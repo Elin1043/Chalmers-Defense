562be9330ee568d8e17501d04de631b2b6cbd84b
Turbobus <59963570+Turbobus@users.noreply.github.com>
2021-10-03 13:14:52 +0200

    Tests for SpawnViruses are now done

--- desktop/src/test/java/testViruses/TestSpawnViruses.java
+++ desktop/src/test/java/testViruses/TestSpawnViruses.java
@@ -8,8 +8,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 /**
  * @author Joel Båtsman Hilmersson
@@ -33,4 +32,35 @@ public class TestSpawnViruses {
         sp.spawnRound(1);
         assertTrue(sp.isSpawning());
     }
+
+    @Test
+    public void testSpawnRound(){
+        sp.spawnRound(1);
+        assertEquals(1, l.size());
+    }
+
+    @Test
+    public void testIfIsSpawningReset(){
+        sp.spawnRound(1);
+        for (int i = 0; i < 100000; i++){       // Very big number because then we can be sure the round should be finished spawning
+            sp.decrementSpawnTimer();
+        }
+        assertFalse(sp.isSpawning());
+    }
+
+    @Test
+    public void testSpawnWholeRound(){
+        sp.spawnRound(1);
+        while(sp.isSpawning()) { sp.decrementSpawnTimer(); }
+        assertEquals(35, l.size());     // If the class has spawned every virus in round data.
+    }
+
+    @Test
+    public void testSpawnRandomRound(){
+        sp.spawnRound(100000);      // Number is over amount of predefined rounds there are. This causes random round to spawn
+        assertTrue(sp.isSpawning());
+    }
+
+    @Test (expected = ArrayIndexOutOfBoundsException.class)
+    public void testSpawnIllegalRound(){ sp.spawnRound(-1); }
 }
