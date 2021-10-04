fbcd83f56bd7d391f2bef13eed9bc73a2177c99e
Turbobus <59963570+Turbobus@users.noreply.github.com>
2021-10-05 01:04:27 +0200

    Test for MechTower is now done to 100%

---
+++ desktop/src/test/java/testTowers/TestMechTower.java
@@ -0,0 +1,30 @@
+package testTowers;
+
+import com.mygdx.chalmersdefense.model.projectiles.IProjectile;
+import com.mygdx.chalmersdefense.model.towers.ITower;
+import com.mygdx.chalmersdefense.model.towers.TowerFactory;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author Joel Båtsman Hilmersson
+ * Test class for MechTower
+ */
+public class TestMechTower {
+
+    @Test
+    public void testCreateProjectile(){
+        List<ITower> addToList = new ArrayList<>();
+        ITower t = TowerFactory.CreateMeck(0, 0, addToList);
+        t.placeTower();
+
+        t.update(new ArrayList<>(), 10, true);
+        assertTrue(addToList.size() > 0);
+    }
+
+}
