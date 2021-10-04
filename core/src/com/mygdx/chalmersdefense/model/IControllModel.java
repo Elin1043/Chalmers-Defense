89b41d6bdea598df218405a1c4dd6b6a9aaad3f4
Turbobus <59963570+Turbobus@users.noreply.github.com>
2021-10-04 15:54:39 +0200

    Added IControllModel interface and implemented it in Model

---
+++ core/src/com/mygdx/chalmersdefense/model/IControllModel.java
@@ -0,0 +1,9 @@
+package com.mygdx.chalmersdefense.model;
+
+/**
+ * @author Joel Båtsman Hilmersson
+ * Interface for model to implement to be controlled by a controller
+ */
+public interface IControllModel {
+
+}
