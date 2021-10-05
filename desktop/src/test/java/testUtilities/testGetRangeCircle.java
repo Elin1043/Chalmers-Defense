0359ddd32bd00ea45a81731400f2a61cfbad5b75
jennycarlsson <44492196+jennycarlsson@users.noreply.github.com>
2021-10-05 21:43:20 +0200

    Added test classes for Calculate, GetRangeCircle and FontFactory

---
+++ desktop/src/test/java/testUtilities/testGetRangeCircle.java
@@ -0,0 +1,22 @@
+package testUtilities;
+
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.mygdx.chalmersdefense.ChalmersDefense;
+import com.mygdx.chalmersdefense.utilities.GetRangeCircle;
+import com.mygdx.chalmersdefense.utilities.PathRectangle;
+
+/**
+ * @author Jenny Carlsson
+ *
+ */
+
+public class testGetRangeCircle {
+
+    LwjglApplication app = new LwjglApplication(new ChalmersDefense());
+    GetRangeCircle getRangeCircle;
+
+
+    
+
+}
