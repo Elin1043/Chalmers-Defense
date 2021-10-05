ddd074b2d7e6a61ef66077693eeb616930b11eb4
Turbobus <59963570+Turbobus@users.noreply.github.com>
2021-10-05 15:58:31 +0200

    Added test for model reset

--- desktop/src/test/java/testModelClasses/TestModel.java
+++ desktop/src/test/java/testModelClasses/TestModel.java
@@ -6,6 +6,7 @@ import com.mygdx.chalmersdefense.model.Model;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -38,12 +39,39 @@ public class TestModel {
         model.dragEnd(100,100,100,100);
         assertTrue(model.getAllMapObjects().size() > 0);
         model.startRoundPressed();
+
         for (int i = 0; i < 1000; i++) {
             model.updateModel();
         }
 
         assertTrue(model.getAllMapObjects().size() > 5);
+    }
 
+    @Test
+    public void testResetModel(){
+        int startCapital = model.getMoney();
+        int startHealth = model.getLivesLeft();
+
+        model.dragStart("smurf",0,0);
+        model.dragEnd(100,100,100,100);
+
+        model.startRoundPressed();
+        assertTrue(model.getAllMapObjects().size() > 0);
+        assertEquals(1, model.getCurrentRound());
+        for (int i = 0; i < 10000; i++) {
+            model.updateModel();
+        }
+        model.startRoundPressed();
+        assertEquals(2, model.getCurrentRound());
+        assertTrue(startCapital > model.getMoney());
+        assertTrue(startHealth > model.getLivesLeft());
+
+        model.resetModel();
+
+        assertEquals(1, model.getCurrentRound());
+        assertEquals(startCapital, model.getMoney());
+        assertEquals(startHealth, model.getLivesLeft());
+        assertEquals(0, model.getAllMapObjects().size());
     }
 
 }
