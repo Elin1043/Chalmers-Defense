d1f61483b1378c80db36e4c67ad209b946e52ba3
Daniel Persson <perssondaniel01@gmail.com>
2021-10-05 20:34:06 +0200

    Moved generic methods and variables to abstract class and made LostPanel subclass of GameScreenOverlay

--- core/src/com/mygdx/chalmersdefense/views/GameScreenViews/LostPanel.java
+++ core/src/com/mygdx/chalmersdefense/views/GameScreenViews/LostPanel.java
@@ -12,23 +12,17 @@ import com.badlogic.gdx.utils.Align;
 import com.mygdx.chalmersdefense.controllers.GameScreenController;
 import com.mygdx.chalmersdefense.utilities.FontFactory;
 
-import static com.badlogic.gdx.graphics.GL20.*;
-
-
 /**
  * @author Daniel Persson
- *
+ * A class used to render an overlay when a player lose the game
  */
-public class LostPanel {
-    private Stage stage;
-    private GameScreenController gameScreenController;
+public class LostPanel extends GameScreenOverlay {
     private final float WIDTH = 810;
     private final float HEIGHT = 400;
 
     private final TextureAtlas lostButtonTexture = new TextureAtlas(Gdx.files.internal("buttons/lostGameButtonSkin/lostGameButtonSkin.atlas")); // Load atlas file from skin
     private final Skin lostButtonSkin = new Skin(Gdx.files.internal("buttons/lostGameButtonSkin/lostGameButtonSkin.json"), lostButtonTexture); // Create skin object
 
-    private final ShapeRenderer shapeRenderer = new ShapeRenderer();
     private final Group lostPanelGroup = new Group();
 
     private final Image backgroundImage = new Image(new Texture("GameScreen/LostPanelBackgroundImage.png"));
@@ -43,22 +37,22 @@ public class LostPanel {
     private final Label tryAgainButtonText = new Label("Try again", FontFactory.getLabelStyle24BlackSemiBold());
 
     public LostPanel(Stage stage, GameScreenController gameScreenController) {
-        this.stage = new Stage(stage.getViewport());
-        this.gameScreenController = gameScreenController;
+        super(gameScreenController, stage);
+
+        initialize();
     }
 
-    public void initialize() {
+    /**
+     * Method used to setup LostPanel GUI components
+     */
+    void initialize() {
         stage.addActor(lostPanelGroup);
-        lostPanelGroup.addActor(backgroundImage);
-        lostPanelGroup.addActor(title);
-        lostPanelGroup.addActor(mainText);
-        lostPanelGroup.addActor(mainMenuButton);
-        mainMenuButton.addActor(mainMenuButtonText);
-        lostPanelGroup.addActor(tryAgainButton);
-        tryAgainButton.addActor(tryAgainButtonText);
+        initializeActors();
 
+        // Set position of background
         backgroundImage.setPosition(stage.getWidth()/2 - WIDTH/2, stage.getHeight()/2 - HEIGHT/2);
 
+        // Set position for main title.
         title.setPosition(
             backgroundImage.getX() + backgroundImage.getWidth() / 2 - title.getWidth() / 2,
             backgroundImage.getY() + backgroundImage.getHeight() - 100);
@@ -67,24 +61,28 @@ public class LostPanel {
         mainText.setWidth(720);
         mainText.setAlignment(Align.center);
         mainText.setPosition(
-                backgroundImage.getX() + backgroundImage.getWidth() / 2 - 720 / 2,
+                backgroundImage.getX() + backgroundImage.getWidth() / 2 - 720/2f,
                 backgroundImage.getY() + backgroundImage.getHeight() - 150);
 
-        gameScreenController.addLostPanelMainMenuClickListener(mainMenuButton);
-        mainMenuButton.setPosition(
-            backgroundImage.getX() + backgroundImage.getWidth()/4 - mainMenuButton.getWidth()/2,
-            backgroundImage.getY() + 65);
-        mainMenuButtonText.setPosition(mainMenuButton.getWidth()/2 - mainMenuButtonText.getWidth()/2, mainMenuButton.getHeight()/2 - mainMenuButtonText.getHeight()/2 + 5);
-
-        gameScreenController.addLostPanelTryAgainClickListener(tryAgainButton);
-        tryAgainButton.setPosition(
-            backgroundImage.getX() + backgroundImage.getWidth() * 3/4 - mainMenuButton.getWidth()/2,
-            backgroundImage.getY() + 65);
-        tryAgainButtonText.setPosition(tryAgainButton.getWidth()/2 - tryAgainButtonText.getWidth()/2, tryAgainButton.getHeight()/2 - tryAgainButtonText.getHeight()/2 + 5);
+        createButtons(backgroundImage, mainMenuButton, mainMenuButtonText, 1);
+        createButtons(backgroundImage, tryAgainButton, tryAgainButtonText, 2);
 
         lostPanelGroup.setVisible(false);
     }
 
+    private void initializeActors() {
+        lostPanelGroup.addActor(backgroundImage);
+        lostPanelGroup.addActor(title);
+        lostPanelGroup.addActor(mainText);
+        lostPanelGroup.addActor(mainMenuButton);
+        mainMenuButton.addActor(mainMenuButtonText);
+        lostPanelGroup.addActor(tryAgainButton);
+        tryAgainButton.addActor(tryAgainButtonText);
+    }
+
+    /**
+     * Renders lost panel overlay
+     */
     public void render() {
         Gdx.input.setInputProcessor(stage);
 
@@ -97,18 +95,7 @@ public class LostPanel {
 
     }
 
-    // Generate gray transparent overlay background
-    private void drawTransparentBackground() {
-        Gdx.gl.glEnable(GL_BLEND);
-        Gdx.gl.glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-        shapeRenderer.setColor(new Color(20 / 255F, 20 / 255F, 20 / 255F, 0.6F));
-        shapeRenderer.rect(0, 0, stage.getWidth(), stage.getHeight());
-        shapeRenderer.end();
-        Gdx.gl.glDisable(GL_BLEND);
-    }
-
-    public void hideLostPanelGroup() {
+    public void hideOverlay() {
         lostPanelGroup.setVisible(false);
     }
 }
