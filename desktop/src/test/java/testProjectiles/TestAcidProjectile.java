21470f694b42fd5f75bdee266e4b0680fc44b825
Elin1043 <47697397+Elin1043@users.noreply.github.com>
2021-10-05 16:01:37 +0200

    Projectiles folder 100% methods test covered

---
+++ desktop/src/test/java/testProjectiles/TestAcidProjectile.java
@@ -0,0 +1,36 @@
+package testProjectiles;
+
+import com.mygdx.chalmersdefense.model.projectiles.IProjectile;
+import com.mygdx.chalmersdefense.model.towers.ITower;
+import com.mygdx.chalmersdefense.model.towers.TowerFactory;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class TestAcidProjectile {
+    List<IProjectile> projectilesList = new ArrayList<>();
+    List<IProjectile> addList = new ArrayList<>();
+    IProjectile projectile;
+
+    @Before
+    public void createProjectile(){
+        ITower tower = TowerFactory.CreateChemist(0,0, addList);
+        tower.placeTower();
+        tower.update(projectilesList, 0, true);
+        projectile = projectilesList.get(0);
+    }
+
+    @Test
+    public void testVirusHit(){
+        assertEquals(0, addList.size());
+        projectile.update(true,0,0);
+        assertTrue(addList.size() > 0);
+    }
+
+}
