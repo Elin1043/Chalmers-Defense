935b19c89ad441874eb3df7c2b2f8b3236a09e27
jennycarlsson <44492196+jennycarlsson@users.noreply.github.com>
2021-10-06 18:47:26 +0200

    Added tests for GetRangeCircle

--- desktop/src/test/java/testUtilities/testGetRangeCircle.java
+++ desktop/src/test/java/testUtilities/testGetRangeCircle.java
@@ -5,6 +5,12 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.mygdx.chalmersdefense.ChalmersDefense;
 import com.mygdx.chalmersdefense.utilities.GetRangeCircle;
 import com.mygdx.chalmersdefense.utilities.PathRectangle;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.awt.*;
+
+import static org.junit.Assert.*;
 
 /**
  * @author Jenny Carlsson
@@ -13,10 +19,26 @@ import com.mygdx.chalmersdefense.utilities.PathRectangle;
 
 public class testGetRangeCircle {
 
-    LwjglApplication app = new LwjglApplication(new ChalmersDefense());
     GetRangeCircle getRangeCircle;
 
+    @Before
+    public void init(){
+        getRangeCircle = new GetRangeCircle();
+    }
 
+    @Test
+    public void testSetAndGetPosition(){
+        getRangeCircle.updatePos(4, 3, 10);
+        assertTrue((getRangeCircle.getX() == 4)
+                    && (getRangeCircle.getY() == 3)
+                    && (getRangeCircle.getRange() == 10));
+    }
+
+    @Test
+    public void testSetAndGetColor(){
+        getRangeCircle.setEnumColor(GetRangeCircle.Color.RED);
+        assertSame(getRangeCircle.getColor(), GetRangeCircle.Color.RED);
+    }
     
 
 }
