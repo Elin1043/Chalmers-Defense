d7173057799f758a795d4a1bfabe6efd5c40503c
Turbobus <59963570+Turbobus@users.noreply.github.com>
2021-10-10 22:57:15 +0200

    Added MatrixProjectile class for use by Hackerman

---
+++ core/src/com/mygdx/chalmersdefense/model/projectiles/MatrixProjectile.java
@@ -0,0 +1,9 @@
+package com.mygdx.chalmersdefense.model.projectiles;
+
+/**
+ * @author Joel Båtsman Hilmersson
+ *
+ * Class representing Hackerman / HackerTowers shooting projectile
+ */
+public class MatrixProjectile {
+}
