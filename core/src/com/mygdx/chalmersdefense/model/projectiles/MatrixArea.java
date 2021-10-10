68577f0e422aac39427c123b6a2e5fc19c7de0de
Turbobus <59963570+Turbobus@users.noreply.github.com>
2021-10-11 01:04:24 +0200

    HackerArea now stays on map for 250 milliseconds and then gets removed

--- core/src/com/mygdx/chalmersdefense/model/projectiles/MatrixArea.java
+++ core/src/com/mygdx/chalmersdefense/model/projectiles/MatrixArea.java
@@ -7,8 +7,24 @@ package com.mygdx.chalmersdefense.model.projectiles;
  */
 public class MatrixArea extends Projectile {
 
+    private int matrixTimer = 50;    // Lifetime of MatrixArea
 
     MatrixArea(float x, float y, int upgradeLevel) {
         super(0, "hackerArea" + upgradeLevel, x, y, 0);
+    }
+
+    @Override
+    public void update(boolean hasVirusBeenHit, int hitVirusHashCode, float angle) {
+        if (matrixTimer <= 0) {
+            this.canRemove = true;
+        } else {
+            matrixTimer--;
+        }
+        super.update(hasVirusBeenHit, hitVirusHashCode, angle);
+    }
+
+    @Override
+    void virusIsHit(int haveHit, float angle) {
+        super.haveHitList.add(haveHit);
     }
 }
