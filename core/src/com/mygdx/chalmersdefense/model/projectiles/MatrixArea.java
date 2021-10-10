15a5713cbb48a226f6d90c5f716ff8eeb86e4665
Turbobus <59963570+Turbobus@users.noreply.github.com>
2021-10-11 00:07:24 +0200

    Added MatrixArea class

---
+++ core/src/com/mygdx/chalmersdefense/model/projectiles/MatrixArea.java
@@ -0,0 +1,9 @@
+package com.mygdx.chalmersdefense.model.projectiles;
+
+/**
+ * @author Joel Båtsman Hilmersson
+ *
+ * Class representing an area effected by the matrix. Slows down viruses that comes in contact
+ */
+public class MatrixArea {
+}
