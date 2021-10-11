d7570bae8bc57889e74c906df2288c5eeff5a402
Turbobus <59963570+Turbobus@users.noreply.github.com>
2021-10-11 12:24:14 +0200

    The slowdown % is now dependent on tower upgrade level

--- core/src/com/mygdx/chalmersdefense/model/projectiles/MatrixArea.java
+++ core/src/com/mygdx/chalmersdefense/model/projectiles/MatrixArea.java
@@ -10,7 +10,7 @@ public class MatrixArea extends Projectile {
     private int matrixTimer = 50;    // Lifetime of MatrixArea
 
     MatrixArea(float x, float y, int upgradeLevel) {
-        super(0, "hackerArea" + upgradeLevel, x, y, 0, 0.5F);
+        super(0, "hackerArea" + upgradeLevel, x, y, 0, 1-(upgradeLevel*0.15F));
     }
 
     @Override
