6ac27c5a92dcbef3b5132d6834a47763de69c638
Daniel <perssondaniel01@gmail.com>
2021-10-11 11:25:39 +0200

    Added PauseMenuOverlay buttons

--- core/src/com/mygdx/chalmersdefense/views/GameScreenViews/PauseMenuOverlay.java
+++ core/src/com/mygdx/chalmersdefense/views/GameScreenViews/PauseMenuOverlay.java
@@ -12,6 +12,12 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.mygdx.chalmersdefense.controllers.GameScreenController;
 import com.mygdx.chalmersdefense.utilities.FontFactory;
 
+/**
+ * @author Jenny Carlsson
+ * @author Daniel Persson
+ *
+ * A class for representing the pause menu in GameScreen
+ */
 public class PauseMenuOverlay extends AbstractOverlay {
 
     private final Group pauseMenuGroup = new Group();
@@ -20,7 +26,7 @@ public class PauseMenuOverlay extends AbstractOverlay {
     private final TextureAtlas pauseMenuButtonTexture = new TextureAtlas(Gdx.files.internal("buttons/pauseMenuButtonSkin/PauseMenuButtonSkin.atlas")); // Load atlas file from skin
     private final Skin pauseMenuButtonSkin = new Skin(Gdx.files.internal("buttons/pauseMenuButtonSkin/PauseMenuButtonSkin.json"), pauseMenuButtonTexture); // Create skin object
 
-    public PauseMenuOverlay(GameScreenController gameScreenController, Stage stage) {
+    public PauseMenuOverlay(Stage stage, GameScreenController gameScreenController) {
         super(gameScreenController, stage);
 
         initialize();
@@ -30,11 +36,18 @@ public class PauseMenuOverlay extends AbstractOverlay {
     void initialize() {
         stage.addActor(pauseMenuGroup);
         pauseMenuGroup.addActor(backgroundImage);
+        backgroundImage.setPosition(stage.getWidth()/2 - backgroundImage.getWidth()/2, stage.getHeight()/2 - backgroundImage.getHeight()/2);
         createButtons();
     }
 
     @Override
     public void render() {
+        Gdx.input.setInputProcessor(stage);
+
+        drawTransparentBackground();
+
+        stage.act();
+        stage.draw();
         pauseMenuGroup.setVisible(true);
     }
 
@@ -46,13 +59,16 @@ public class PauseMenuOverlay extends AbstractOverlay {
     private void createButtons() {
         String[] buttonTexts = {"Continue", "Settings", "Quit"};
 
-        for (String buttonText : buttonTexts) {
+        for (int i = 0; i < buttonTexts.length; i++) {
             Button button = new Button(pauseMenuButtonSkin);
             pauseMenuGroup.addActor(button);
+            button.setPosition(
+                    backgroundImage.getX() + backgroundImage.getWidth() / 2 - button.getWidth() / 2,
+                    backgroundImage.getY() + backgroundImage.getHeight() - (button.getHeight() + 10) * (i + 1));
 
-            Label buttonLabel = new Label(buttonText, FontFactory.getLabelStyle20Black());
-            pauseMenuGroup.addActor(buttonLabel);
-            buttonLabel.setPosition(button.getX() + button.getWidth()/2 - buttonLabel.getWidth()/2, button.getY() + button.getHeight()/2 - buttonLabel.getHeight()/2);
+            Label buttonLabel = new Label(buttonTexts[i], FontFactory.getLabelStyle20Black());
+            button.addActor(buttonLabel);
+            buttonLabel.setPosition(button.getWidth() / 2 - buttonLabel.getWidth() / 2, button.getHeight() / 2 - buttonLabel.getHeight() / 2);
         }
     }
 }
