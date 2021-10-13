5a20cdd1b8f24ed1453ccb301c2615db203c2fb4
Turbobus <59963570+Turbobus@users.noreply.github.com>
2021-10-13 23:55:28 +0200

    AcidPool class now uses CountDownTimer

--- core/src/com/mygdx/chalmersdefense/model/projectiles/AcidPool.java
+++ core/src/com/mygdx/chalmersdefense/model/projectiles/AcidPool.java
@@ -1,14 +1,19 @@
 package com.mygdx.chalmersdefense.model.projectiles;
 
 
+import com.mygdx.chalmersdefense.utilities.CountDownTimer;
+
 /**
  * @author Joel Båtsman Hilmersson
  * Class representing an acidPool projectile
  */
 class AcidPool extends Projectile{
 
-    private int poolTimer = 150;    // Max Lifetime off acid pool
-    private int maxVirusHit = 5;    // Max amount of virus that can be hit with the pool
+    //private int poolTimer = 150;    // Max Lifetime off acid pool
+    private final CountDownTimer poolTimer = new CountDownTimer(150);
+
+    //private int maxVirusHit = 5;    // Max amount of virus that can be hit with the pool
+    private final CountDownTimer maxVirusHit = new CountDownTimer(5);
 
     AcidPool(float x, float y, int upgradeLevel) {
         super(0, "chemistAcid" + upgradeLevel, x, y, 0);
@@ -16,22 +21,13 @@ class AcidPool extends Projectile{
 
     @Override
     public void update(boolean hasVirusBeenHit, int hitVirusHashCode, float angle) {
-        if (poolTimer <= 0) {
-            this.canRemove = true;
-        } else {
-            poolTimer--;
-        }
+        if (poolTimer.haveReachedZero()) { this.canRemove = true; }
         super.update(hasVirusBeenHit, hitVirusHashCode, angle);
     }
 
     @Override
     void virusIsHit(int haveHit, float angle) {
-        if (maxVirusHit > 0) {
-            maxVirusHit--;
-        } else {
-            this.canRemove = true;
-        }
-
+        if (maxVirusHit.haveReachedZero()) { this.canRemove = true; }
         super.haveHitList.add(haveHit);
     }
 }
