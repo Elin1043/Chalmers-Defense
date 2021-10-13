5d6436923ac0d008ca1d594ee1442c0bd73cc18a
Turbobus <59963570+Turbobus@users.noreply.github.com>
2021-10-13 22:17:11 +0200

    Created method in ObjectTimer

--- core/src/com/mygdx/chalmersdefense/utilities/ObjectTimer.java
+++ core/src/com/mygdx/chalmersdefense/utilities/ObjectTimer.java
@@ -6,4 +6,21 @@ package com.mygdx.chalmersdefense.utilities;
  */
 public class ObjectTimer {
 
+    private final int timerLength;  // The total length of the timer
+    private int timerCountDown;     // The current timer cooldown/position
+
+    public ObjectTimer(int timerLength){
+        this.timerLength = timerLength;
+    }
+
+
+    public boolean updateTimer(){   // TODO Question, is this okay? I both modify timer and return a boolean if the timer is done or should they be separated
+        if (timerCountDown > 0){
+            timerCountDown--;
+            return false;
+        } else {
+            timerCountDown = timerLength;   // Resets timer
+            return true;
+        }
+    }
 }
