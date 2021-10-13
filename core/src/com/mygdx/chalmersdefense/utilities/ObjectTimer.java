f8d52b6acbb2e5b1c0145a85f464be3b75d38f77
Turbobus <59963570+Turbobus@users.noreply.github.com>
2021-10-13 22:06:24 +0200

    Created ObjectTimer class

---
+++ core/src/com/mygdx/chalmersdefense/utilities/ObjectTimer.java
@@ -0,0 +1,9 @@
+package com.mygdx.chalmersdefense.utilities;
+
+/**
+ * @author Joel Båtsman Hilmersson
+ * Class for holding a simple countdown timer used by many classes
+ */
+public class ObjectTimer {
+
+}
