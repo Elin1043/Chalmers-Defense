cbbc1495f24f43cf0b876a5eb3e634d58c1fcc61
Daniel <perssondaniel01@gmail.com>
2021-10-15 00:19:17 +0200

    Cleaned up code

--- core/src/com/mygdx/chalmersdefense/views/overlays/WinPanelOverlay.java
+++ core/src/com/mygdx/chalmersdefense/views/overlays/WinPanelOverlay.java
@@ -21,24 +21,14 @@ public class WinPanelOverlay extends AbstractOverlay {
     private final float WIDTH = 810;
     private final float HEIGHT = 400;
 
-    private final WinPanelOverlayController winPanelOverlayController;
+    private final WinPanelOverlayController winPanelOverlayController; // Controller used for adding listeners
 
     private final TextureAtlas winButtonTexture = new TextureAtlas(Gdx.files.internal("buttons/winGameButtonSkin/winGameButtonSkin.atlas")); // Load atlas file from skin
     private final Skin winButtonSkin = new Skin(Gdx.files.internal("buttons/winGameButtonSkin/winGameButtonSkin.json"), winButtonTexture); // Create skin object
 
-    private final Group winPanelGroup = new Group();
+    private final Group winPanelGroup = new Group(); // Group to att all actors to
 
-    private final Image backgroundImage = new Image(new Texture("GameScreen/WinPanelBackgroundImage.png"));
-
-    private final Label title = new Label("YOU WON!", FontFactory.getLabelStyle36BlackBold());
-    private final Label mainText = new Label("You have defended Chalmers from the evil Corona viruses. \n" +
-            "You can now continue to play if you want to decimate the viruses more or return to the main menu ", FontFactory.getLabelStyle18Black());
-
-    private final Button mainMenuButton = new Button(winButtonSkin);
-    private final Label mainMenuButtonText = new Label("Main menu", FontFactory.getLabelStyle24BlackSemiBold());
-
-    private final Button continueButton = new Button(winButtonSkin);
-    private final Label continueButtonText = new Label("Continue", FontFactory.getLabelStyle24BlackSemiBold());
+    private final Image backgroundImage = new Image(new Texture("GameScreen/WinPanelBackgroundImage.png")); // Background image of overlay
 
     public WinPanelOverlay(WinPanelOverlayController winPanelOverlayController) {
         this.winPanelOverlayController = winPanelOverlayController;
@@ -47,12 +37,34 @@ public class WinPanelOverlay extends AbstractOverlay {
     @Override
     protected void initialize() {
         stage.addActor(winPanelGroup);
-        initializeActors();
+        winPanelGroup.addActor(backgroundImage);
 
         // Set position of background
         backgroundImage.setPosition(stage.getWidth() / 2 - WIDTH / 2, stage.getHeight() / 2 - HEIGHT / 2);
 
-        // Set position for main title.
+        // Create labels
+        createLabels();
+
+        // Create first button
+        Button mainMenuButton = new Button(winButtonSkin);
+        Label mainMenuButtonText = new Label("Main menu", FontFactory.getLabelStyle24BlackSemiBold());
+        createButtons(backgroundImage, mainMenuButton, mainMenuButtonText, 1);
+
+        // Create second button
+        Button continueButton = new Button(winButtonSkin);
+        Label continueButtonText = new Label("Continue", FontFactory.getLabelStyle24BlackSemiBold());
+        createButtons(backgroundImage, continueButton, continueButtonText, 2);
+
+        winPanelGroup.setVisible(false);
+    }
+
+    private void createLabels() {
+        Label title = new Label("YOU WON!", FontFactory.getLabelStyle36BlackBold());
+        Label mainText = new Label("You have defended Chalmers from the evil Corona viruses. \n" +
+                "You can now continue to play if you want to decimate the viruses more or return to the main menu ", FontFactory.getLabelStyle18Black());
+        winPanelGroup.addActor(title);
+        winPanelGroup.addActor(mainText);
+
         title.setPosition(
                 backgroundImage.getX() + backgroundImage.getWidth() / 2 - title.getWidth() / 2,
                 backgroundImage.getY() + backgroundImage.getHeight() - 100);
@@ -63,21 +75,6 @@ public class WinPanelOverlay extends AbstractOverlay {
         mainText.setPosition(
                 backgroundImage.getX() + backgroundImage.getWidth() / 2 - 720 / 2f,
                 backgroundImage.getY() + backgroundImage.getHeight() - 150);
-
-        createButtons(backgroundImage, mainMenuButton, mainMenuButtonText, 1);
-        createButtons(backgroundImage, continueButton, continueButtonText, 2);
-
-        winPanelGroup.setVisible(false);
-    }
-
-    private void initializeActors() {
-        winPanelGroup.addActor(backgroundImage);
-        winPanelGroup.addActor(title);
-        winPanelGroup.addActor(mainText);
-        winPanelGroup.addActor(mainMenuButton);
-        mainMenuButton.addActor(mainMenuButtonText);
-        winPanelGroup.addActor(continueButton);
-        continueButton.addActor(continueButtonText);
     }
 
     @Override
@@ -117,5 +114,7 @@ public class WinPanelOverlay extends AbstractOverlay {
                 backgroundImage.getX() + (backgroundImage.getWidth() * offsetMulX) - button.getWidth() / 2,
                 backgroundImage.getY() + 65);
         buttonLabel.setPosition(button.getWidth() / 2 - buttonLabel.getWidth() / 2, button.getHeight() / 2 - buttonLabel.getHeight() / 2 + 5);
+        winPanelGroup.addActor(button);
+        button.addActor(buttonLabel);
     }
 }
