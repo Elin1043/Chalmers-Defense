70f841c6bf2c4af258cd84d7225b155b40dc5992
Daniel Persson <perssondaniel01@gmail.com>
2021-10-16 15:22:37 +0200

    Added overlay render functionality

--- core/src/com/mygdx/chalmersdefense/views/MainScreen.java
+++ core/src/com/mygdx/chalmersdefense/views/MainScreen.java
@@ -10,6 +10,9 @@ import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.mygdx.chalmersdefense.controllers.MainScreenController;
+import com.mygdx.chalmersdefense.model.IViewModel;
+import com.mygdx.chalmersdefense.views.overlays.AbstractOverlay;
+import com.mygdx.chalmersdefense.views.overlays.OverlayManager;
 
 /**
  * @author Daniel Persson
@@ -19,18 +22,21 @@ public class MainScreen extends AbstractScreen {
     Image img;
 
     private ImageButton playButton;
+    private Button settingsButton;
 
     private final TextureAtlas quitButtonAtlas = new TextureAtlas(Gdx.files.internal("buttons/quitButtonSkin/QuitButtonSkin.atlas")); // Load atlas file from skin
     private final Skin quitButtonSkin = new Skin(Gdx.files.internal("buttons/quitButtonSkin/QuitButtonSkin.json"), quitButtonAtlas); // Create skin object
     private final Button quitButton = new Button(quitButtonSkin);
 
     private final MainScreenController mainScreenController;
+    private final IViewModel model;
 
-    public MainScreen(MainScreenController mainScreenController) {
+    public MainScreen(IViewModel model, MainScreenController mainScreenController) {
         super();
+        this.model = model;
         this.mainScreenController = mainScreenController;
         img = new Image(new Texture("HomeScreen.png"));
-        createPlayButton();
+
     }
 
     private void createPlayButton() {
@@ -45,15 +51,40 @@ public class MainScreen extends AbstractScreen {
 
         //Add the button to the stage to perform rendering and take input. (WILL BE MOVED)
         mainScreenController.addPlayButtonListener(playButton);
+    }
 
+    private void createSettingsButton() {
+        TextureAtlas settingsButtonAtlas = new TextureAtlas(Gdx.files.internal("buttons/settingsButtonSkin/SettingsButtonSkin.atlas")); // Load atlas file from skin
+        Skin settingsButtonSkin = new Skin(Gdx.files.internal("buttons/settingsButtonSkin/SettingsButtonSkin.json"), settingsButtonAtlas); // Create skin object
+        settingsButton = new Button(settingsButtonSkin);
+        mainScreenController.addSettingsButtonClickListener(settingsButton);
+        settingsButton.setPosition(430,110);
     }
 
     @Override
     public void buildStage() {
-        super.render(Gdx.graphics.getDeltaTime());
+        createPlayButton();
+        createSettingsButton();
         addActor(img);
         addActor(playButton);
         addActor(quitButton);
+        addActor(settingsButton);
+    }
 
+    /**
+     * Renders GameScreen to screen
+     *
+     * @param delta the timeframe from previous frame to current frame
+     */
+    @Override
+    public void render(float delta) {
+        super.render(Gdx.graphics.getDeltaTime());
+        Gdx.input.setInputProcessor(this);
+
+        OverlayManager.getInstance().showOverlay(model.showOverlay());
+        AbstractOverlay abstractOverlay = OverlayManager.getInstance().getCurrentOverlay();
+        if (abstractOverlay != null) {
+            abstractOverlay.render();
+        }
     }
 }
