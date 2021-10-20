f6d5e19f76f69c2bfec5ed1f51cf14bc54c051fb
jennycarlsson <44492196+jennycarlsson@users.noreply.github.com>
2021-10-20 17:03:06 +0200

    Added info button on main screen

--- core/src/com/mygdx/chalmersdefense/views/MainScreen.java
+++ core/src/com/mygdx/chalmersdefense/views/MainScreen.java
@@ -23,6 +23,7 @@ public class MainScreen extends AbstractScreen {
 
     private ImageButton playButton;
     private Button settingsButton;
+    private Button infoButton;
 
     private final TextureAtlas quitButtonAtlas = new TextureAtlas(Gdx.files.internal("buttons/quitButtonSkin/QuitButtonSkin.atlas")); // Load atlas file from skin
     private final Skin quitButtonSkin = new Skin(Gdx.files.internal("buttons/quitButtonSkin/QuitButtonSkin.json"), quitButtonAtlas); // Create skin object
@@ -39,10 +40,12 @@ public class MainScreen extends AbstractScreen {
 
         createPlayButton();
         createSettingsButton();
+        createInfoButton();
         addActor(img);
         addActor(playButton);
         addActor(quitButton);
         addActor(settingsButton);
+        addActor(infoButton);
     }
 
     private void createPlayButton() {
@@ -67,6 +70,14 @@ public class MainScreen extends AbstractScreen {
         settingsButton.setPosition(430,110);
     }
 
+    private void createInfoButton() {
+        TextureAtlas infoButtonAtlas = new TextureAtlas(Gdx.files.internal("buttons/infoButtonSkin/InfoButtonSkin.atlas")); // Load atlas file from skin
+        Skin infoButtonSkin = new Skin(Gdx.files.internal("buttons/infoButtonSkin/InfoButtonSkin.json"), infoButtonAtlas); // Create skin object
+        infoButton = new Button(infoButtonSkin);
+        mainScreenController.addInfoButtonClickListener(infoButton);
+        infoButton.setPosition(1140,110);
+    }
+
     /**
      * Renders GameScreen to screen
      *
@@ -82,4 +93,6 @@ public class MainScreen extends AbstractScreen {
             abstractOverlay.render();
         }
     }
+
+
 }
