18d7302abd1fb53d9b7dca50bd71a3cdea0c34d0
Elin1043 <47697397+Elin1043@users.noreply.github.com>
2021-10-20 10:25:16 +0200

    Virus classes now tested 100% on methods again

--- desktop/src/test/java/testViruses/TestVirus.java
+++ desktop/src/test/java/testViruses/TestVirus.java
@@ -6,6 +6,9 @@ import com.mygdx.chalmersdefense.model.viruses.IVirus;
 import com.mygdx.chalmersdefense.model.viruses.VirusFactory;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 /**
@@ -83,6 +86,37 @@ public class TestVirus {
             v1.update();
             v2.update();
         }
+
+        assertTrue(v1.getTotalDistanceTraveled() < v2.getTotalDistanceTraveled());
+    }
+
+    @Test
+    public void testVirusBossVirusDecreaseHealth(){
+        List<IVirus> virusList = new ArrayList<>();
+        IVirus v1 = VirusFactory.createBossVirus(virusList);
+
+        for (int i = 0; i < 5; i++){
+            v1.update();
+        }
+
+        v1.decreaseHealth(50F);
+
+
+        assertEquals(5, virusList.size());
+    }
+
+    @Test
+    public void testVirusBossVirusSlowDownEffect(){
+        List<IVirus> virusList = new ArrayList<>();
+        IVirus v1 = VirusFactory.createBossVirus(virusList);
+        IVirus v2 = VirusFactory.createBossVirus(virusList);
+
+        v1.decreaseHealth(0.7F);
+        for (int i = 0; i < 10; i++){
+            v1.update();
+            v2.update();
+        }
+
 
         assertTrue(v1.getTotalDistanceTraveled() < v2.getTotalDistanceTraveled());
     }
