bbce6142ec0366b9f4d0e1001c2419d0a9a5ac8d
jennycarlsson <44492196+jennycarlsson@users.noreply.github.com>
2021-10-20 21:34:43 +0200

    Added label with title of info

--- core/src/com/mygdx/chalmersdefense/views/overlays/InfoOverlay.java
+++ core/src/com/mygdx/chalmersdefense/views/overlays/InfoOverlay.java
@@ -4,18 +4,28 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.utils.Align;
 import com.mygdx.chalmersdefense.controllers.overlays.AbstractOverlayController;
 import com.mygdx.chalmersdefense.controllers.overlays.InfoOverlayController;
 import com.mygdx.chalmersdefense.controllers.overlays.PauseMenuOverlayController;
+import com.mygdx.chalmersdefense.utilities.FontFactory;
+
+/**
+ * @author Jenny Carlsson
+ * A class to display info overlay
+ */
 
 public class InfoOverlay extends AbstractOverlay {
+    private final InfoOverlayController infoOverlayController;
 
     private final Group infoGroup = new Group();
 
     private final Image backgroundImage = new Image(new Texture("GameScreen/overlays/SettingsBackgroundImage.png"));
 
-    public InfoOverlay(AbstractOverlayController abstractOverlayController) {
+    public InfoOverlay(AbstractOverlayController abstractOverlayController, InfoOverlayController infoOverlayController) {
         super(abstractOverlayController);
+        this.infoOverlayController = infoOverlayController;
     }
 
     @Override
@@ -26,6 +36,11 @@ public class InfoOverlay extends AbstractOverlay {
 
             backgroundImage.setPosition(stage.getWidth()/2 - backgroundImage.getWidth()/2, stage.getHeight()/2 - backgroundImage.getHeight()/2);
             ImageButton exitButton = createExitPauseMenuButton(infoGroup, backgroundImage);
+            infoOverlayController.addExitPauseMenuButtonClickListener(exitButton);
+
+            Label infoTitleLabel = new Label("Game info", FontFactory.getLabelStyle36BlackBold());
+            infoGroup.addActor(infoTitleLabel);
+            infoTitleLabel.setPosition(backgroundImage.getX() + (backgroundImage.getWidth() / 2 - infoTitleLabel.getWidth() / 2), backgroundImage.getY() + 320);
         }
     }
 
