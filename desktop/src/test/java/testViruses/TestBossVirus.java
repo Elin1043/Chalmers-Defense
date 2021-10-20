41250d03095512ac263f332c94c5c7295ab7549b
Turbobus <59963570+Turbobus@users.noreply.github.com>
2021-10-20 11:17:13 +0200

    Moved BossVirus test methods to own class

---
+++ desktop/src/test/java/testViruses/TestBossVirus.java
@@ -0,0 +1,48 @@
+package testViruses;
+
+import com.mygdx.chalmersdefense.model.viruses.IVirus;
+import com.mygdx.chalmersdefense.model.viruses.VirusFactory;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author Elin Forsberg
+ * Test class for testing BossVirus
+ */
+public class TestBossVirus {
+
+    @Test
+    public void testVirusBossVirusDecreaseHealth(){
+        List<IVirus> virusList = new ArrayList<>();
+        IVirus v1 = VirusFactory.createBossVirus(virusList);
+
+        for (int i = 0; i < 5; i++){
+            v1.update();
+        }
+
+        v1.decreaseHealth(50F);
+
+
+        assertEquals(5, virusList.size());
+    }
+
+    @Test
+    public void testVirusBossVirusSlowDownEffect(){
+        List<IVirus> virusList = new ArrayList<>();
+        IVirus v1 = VirusFactory.createBossVirus(virusList);
+        IVirus v2 = VirusFactory.createBossVirus(virusList);
+
+        v1.decreaseHealth(0.7F);
+        for (int i = 0; i < 10; i++){
+            v1.update();
+            v2.update();
+        }
+
+        assertTrue(v1.getTotalDistanceTraveled() < v2.getTotalDistanceTraveled());
+    }
+}
--- desktop/src/test/java/testViruses/TestVirus.java
+++ desktop/src/test/java/testViruses/TestVirus.java
@@ -89,36 +89,4 @@ public class TestVirus {
 
         assertTrue(v1.getTotalDistanceTraveled() < v2.getTotalDistanceTraveled());
     }
-
-    @Test
-    public void testVirusBossVirusDecreaseHealth(){
-        List<IVirus> virusList = new ArrayList<>();
-        IVirus v1 = VirusFactory.createBossVirus(virusList);
-
-        for (int i = 0; i < 5; i++){
-            v1.update();
-        }
-
-        v1.decreaseHealth(50F);
-
-
-        assertEquals(5, virusList.size());
-    }
-
-    @Test
-    public void testVirusBossVirusSlowDownEffect(){
-        List<IVirus> virusList = new ArrayList<>();
-        IVirus v1 = VirusFactory.createBossVirus(virusList);
-        IVirus v2 = VirusFactory.createBossVirus(virusList);
-
-        v1.decreaseHealth(0.7F);
-        for (int i = 0; i < 10; i++){
-            v1.update();
-            v2.update();
-        }
-
-
-        assertTrue(v1.getTotalDistanceTraveled() < v2.getTotalDistanceTraveled());
-    }
-
 }
