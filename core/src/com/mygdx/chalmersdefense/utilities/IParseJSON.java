4c5ff0d65aef55fcfb5c12f00be170f33392e95e
Daniel <perssondaniel01@gmail.com>
2021-10-22 00:58:45 +0200

    Added exceptions to javadoc

--- core/src/com/mygdx/chalmersdefense/utilities/IParseJSON.java
+++ core/src/com/mygdx/chalmersdefense/utilities/IParseJSON.java
@@ -17,6 +17,7 @@ public interface IParseJSON {
      * Navigates trough JSON and sets currentJsonValue to a new JSON value depending on inputted key
      * @param key to JSON value
      * @return this object to enable method chaining
+     * @throws NullPointerException if inputted key leads to a null object
      */
     IParseJSON navThroughJSON(String key) throws NullPointerException;
 
@@ -24,6 +25,7 @@ public interface IParseJSON {
      * Sets currently JSON value to a new JSON value depending on inputted index
      * @param index to get JSON value from
      * @return this object to enable method chaining
+     * @throws NullPointerException if inputted index leads to a null object
      */
     IParseJSON getByIndex(int index) throws NullPointerException;
 
@@ -31,20 +33,23 @@ public interface IParseJSON {
      * Gets String value of current JSON value based on inputted key
      * @param key to JSON value
      * @return a String of a JSON value
+     * @throws IllegalArgumentException if inputted key leads to a null object
      */
-    String getString(String key);
+    String getString(String key) throws IllegalArgumentException;
 
     /**
      * Gets Integer value of current JSON value based on inputted key
      * @param key to JSON value
      * @return a Integer of a JSON value
+     * @throws IllegalArgumentException if inputted key leads to a null object
      */
-    Integer getInteger(String key);
+    Integer getInteger(String key) throws IllegalArgumentException;
 
     /**
      * Gets multiple Double values based on multiple inputted keys
      * @param keys an infinite amount of keys
      * @return a HashMap with Double values of current JSON value
+     * @throws IllegalArgumentException if inputted keys leads to a null object
      */
-    HashMap<String, Double> getDoubleHashMap(String... keys);
+    HashMap<String, Double> getDoubleHashMap(String... keys) throws IllegalArgumentException;
 }
