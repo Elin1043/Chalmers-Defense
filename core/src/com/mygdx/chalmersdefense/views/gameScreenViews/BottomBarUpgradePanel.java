cd0382118aa9bcc588023c91744c9cc0c1df514d
Turbobus <59963570+Turbobus@users.noreply.github.com>
2021-10-23 01:49:44 +0200

    Fixed upgrade bug in the game

--- core/src/com/mygdx/chalmersdefense/views/gameScreenViews/BottomBarUpgradePanel.java
+++ core/src/com/mygdx/chalmersdefense/views/gameScreenViews/BottomBarUpgradePanel.java
@@ -240,7 +240,7 @@ final public class BottomBarUpgradePanel {
             upgradeButton.setChecked(false);
             upgradeButton.setTouchable(Touchable.enabled);
 
-            // If player can afford upgrade enable button
+            // If player can't afford upgrade disable button
             if (cantAfford) {
                 upgradeButton.setDisabled(true);
                 upgradeButton.setTouchable(Touchable.disabled);
@@ -251,9 +251,9 @@ final public class BottomBarUpgradePanel {
         }
 
         // Modify second button only
-        if (buttonNr == 2)
+        if (buttonNr == 2) {
             updateSecondUpgradeButton(towerUpgradeLevel, upgradeButton, upgradedTowerSprite, cantAfford, upgradeIsBought);
-
+        }
 
         batch.begin();
         upgradedTowerSprite.draw(batch);
@@ -268,6 +268,7 @@ final public class BottomBarUpgradePanel {
     }
 
     private void updateSecondUpgradeButton(int towerUpgradeLevel, Button upgradeButton, Sprite upgradedTowerSprite, boolean cantAfford, boolean upgradeIsBought) {
+
         if (!upgradeIsBought) {
             // If first upgrade not bought disable second button
             if (towerUpgradeLevel == 1) {
@@ -277,6 +278,7 @@ final public class BottomBarUpgradePanel {
                 upgradedTowerSprite.setColor(Color.LIGHT_GRAY);
                 // If first upgrade is bought enable second upgrade button
             } else if (towerUpgradeLevel >= 2) {
+                upgradeButton.setDisabled(false);
                 upgradeButton.setTouchable(Touchable.enabled);
                 upgradeButton.setColor(Color.WHITE);
                 upgradedTowerSprite.setColor(Color.WHITE);
@@ -288,6 +290,11 @@ final public class BottomBarUpgradePanel {
                     upgradeButton.setTouchable(Touchable.disabled);
                 }
             }
+        } else { //
+            upgradeButton.setDisabled(false);
+            upgradeButton.setTouchable(Touchable.enabled);
+            upgradeButton.setColor(Color.WHITE);
+            upgradedTowerSprite.setColor(Color.WHITE);
         }
     }
 }
