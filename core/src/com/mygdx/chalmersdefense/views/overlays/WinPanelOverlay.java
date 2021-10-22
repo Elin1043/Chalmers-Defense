27c3650c14065db5d5a09e8fef0744348dba5979
Daniel Persson <perssondaniel01@gmail.com>
2021-10-22 10:48:50 +0200

    Removed code duplication and moved common methods to AbstractOverlay

--- core/src/com/mygdx/chalmersdefense/views/overlays/WinPanelOverlay.java
+++ core/src/com/mygdx/chalmersdefense/views/overlays/WinPanelOverlay.java
@@ -46,41 +46,28 @@ final class WinPanelOverlay extends AbstractOverlay {
         backgroundImage.setPosition(stage.getWidth() / 2 - backgroundImage.getWidth() / 2, stage.getHeight() / 2 - backgroundImage.getHeight() / 2);
 
         // Create labels
-        createLabels();
+        Label title = new Label("YOU WON!", FontFactory.getLabelStyle36BlackBold());
+        Label mainText = new Label("You have defended Chalmers from the evil Corona viruses. \n" +
+                "You can now continue to play if you want to decimate the viruses more or return to the main menu ", FontFactory.getLabelStyle18Black());
+        setupWinOrLoseOverlayLabels(backgroundImage, title, mainText);
+        winPanelGroup.addActor(title);
+        winPanelGroup.addActor(mainText);
 
         // Create first button
         Button mainMenuButton = new Button(winButtonSkin);
         Label mainMenuButtonText = new Label("Main menu", FontFactory.getLabelStyle24BlackSemiBold());
-        createButtons(backgroundImage, mainMenuButton, mainMenuButtonText, 1);
+        setupWinAndLostOverlayButtons(backgroundImage, mainMenuButton, mainMenuButtonText, 1);
+        winPanelGroup.addActor(mainMenuButton);
+        abstractOverlayController.addMainMenuClickListener(mainMenuButton);
 
         // Create second button
         Button continueButton = new Button(winButtonSkin);
         Label continueButtonText = new Label("Continue", FontFactory.getLabelStyle24BlackSemiBold());
-        createButtons(backgroundImage, continueButton, continueButtonText, 2);
+        setupWinAndLostOverlayButtons(backgroundImage, continueButton, continueButtonText, 2);
+        winPanelGroup.addActor(continueButton);
+        winPanelOverlayController.addWinPanelContinueClickListener(continueButton);
 
         winPanelGroup.setVisible(false);
-    }
-
-    /**
-     * Creates all labels
-     */
-    private void createLabels() {
-        Label title = new Label("YOU WON!", FontFactory.getLabelStyle36BlackBold());
-        Label mainText = new Label("You have defended Chalmers from the evil Corona viruses. \n" +
-                "You can now continue to play if you want to decimate the viruses more or return to the main menu ", FontFactory.getLabelStyle18Black());
-        winPanelGroup.addActor(title);
-        winPanelGroup.addActor(mainText);
-
-        title.setPosition(
-                backgroundImage.getX() + backgroundImage.getWidth() / 2 - title.getWidth() / 2,
-                backgroundImage.getY() + backgroundImage.getHeight() - 100);
-
-        mainText.setWrap(true);
-        mainText.setWidth(720);
-        mainText.setAlignment(Align.center);
-        mainText.setPosition(
-                backgroundImage.getX() + backgroundImage.getWidth() / 2 - 720 / 2f,
-                backgroundImage.getY() + backgroundImage.getHeight() - 170);
     }
 
     @Override
@@ -92,29 +79,5 @@ final class WinPanelOverlay extends AbstractOverlay {
    @Override
     public void hideOverlay() {
         winPanelGroup.setVisible(false);
-    }
-
-    /**
-     * Setup a new button
-     * @param backgroundImage of the new button
-     * @param button the button to be setup
-     * @param buttonLabel of the new button
-     * @param buttonNr of the new button
-     */
-    private void createButtons(Image backgroundImage, Button button, Label buttonLabel, int buttonNr) {
-        // Offset used to place button in center of left or right part.
-        float offsetMulX = buttonNr == 1 ? 1 / 4f : 3/ 4f;
-        if (buttonNr == 1) {
-            abstractOverlayController.addMainMenuClickListener(button);
-        } else {
-            winPanelOverlayController.addWinPanelContinueClickListener(button);
-        }
-
-        button.setPosition(
-                backgroundImage.getX() + (backgroundImage.getWidth() * offsetMulX) - button.getWidth() / 2,
-                backgroundImage.getY() + 65);
-        buttonLabel.setPosition(button.getWidth() / 2 - buttonLabel.getWidth() / 2, button.getHeight() / 2 - buttonLabel.getHeight() / 2 + 5);
-        winPanelGroup.addActor(button);
-        button.addActor(buttonLabel);
     }
 }
